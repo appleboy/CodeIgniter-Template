2bfa8498138219285786f6ba72b4bcf429bf33fc
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-01-26 14:39:13 +0800

    upload template library

---
+++ config/template.php
@@ -0,0 +1,42 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * default layout
+ * Location: application/views/
+ */
+$config['template_layout'] = 'template/layout';
+
+/**
+ * default css
+ */
+$config['template_css'] = array(
+    '/assets/css/index.css' => 'screen'
+);
+
+/**
+ * default javascript
+ * load javascript on header: FALSE
+ * load javascript on footer: TRUE
+ */
+$config['template_js'] = array(
+    'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' => FALSE,
+    '/assets/js/index.js' => TRUE
+);
+
+/**
+ * default variable
+ */
+$config['template_vars'] = array(
+    'site_description' => 'xxxx',
+    'site_keywords' => 'xxxx'
+);
+
+/**
+ * default site title
+ */
+$config['base_title'] = 'xxxxx';
+
+/**
+ * default title separator
+ */
+$config['title_separator'] = ' | ';
