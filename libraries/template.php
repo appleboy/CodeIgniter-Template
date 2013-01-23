f9170d95414cad28a0f321c54cda01cc9cf90910
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-01-23 19:13:23 +0800

    merge master
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- libraries/template.php
+++ libraries/template.php
@@ -81,6 +81,7 @@ class Template
     public function __construct($config = array())
     {
         $this->_ci = get_instance();
+        $this->_ci->load->helper('html');
 
         log_message('debug', 'Tempalte Class Initialized');
 
@@ -268,8 +269,13 @@ class Template
      * @param  boolean $return
      * @return string
      */
-    public function render($view, $data = NULL, $return = FALSE)
+    public function render($view, $data = array(), $return = FALSE)
     {
+        // check $data is array()
+        if (!is_array($data)) {
+            $data = array();
+        }
+
         // merge template variable
         $data = array_merge($data, $this->_data);
 
