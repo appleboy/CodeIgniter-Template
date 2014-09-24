4321b9cbb5c2f403ebb16b5aac033a6cb1956747
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-09-24 15:13:19 +0800

    update template default layout,
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- template/layout.php
+++ template/layout.php
@@ -1,16 +1,12 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--><html class="no-js" lang="<?php echo $lang; ?>"> <!--<![endif]-->
-
+<html class="no-js" lang="<?php echo $lang; ?>">
 <head>
-<meta charset="<?php echo $meta_charset; ?>">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title><?php echo $site_title; ?></title>
 <meta name="description" content="<?php echo $site_description; ?>" />
 <meta name="keywords" content="<?php echo $site_keywords; ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php echo $meta_tag; ?>
 <?php echo $styles; ?>
 <!-- JS -->
@@ -21,5 +17,14 @@
         <?php echo $content; ?>
     </div>
     <?php echo $scripts_footer; ?>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
 </body>
 </html>
